89051a099d9947c219bafa88bc9c33e498574f0b
JoA Azria <joa.azria@gmail.com>
2020-10-27 20:58:46 -0300

    Models Creações

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_011027) do
+ActiveRecord::Schema.define(version: 2020_10_27_235434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "cohemp_id", null: false
+    t.date "date"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cohemp_id"], name: "index_bookings_on_cohemp_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "cohemps", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "adress"
+    t.boolean "availability"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cohemps_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +48,7 @@ ActiveRecord::Schema.define(version: 2020_10_27_011027) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cohemps"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "cohemps", "users"
 end
