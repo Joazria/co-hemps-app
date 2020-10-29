352d566a59669f737f391e8e18a8a4efd560c8ac
JoA Azria <joa.azria@gmail.com>
2020-10-29 19:43:27 -0300

    models validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :cohemp
+  COHEMPS_STATUS = ["available", "canceled", "confirmed"]
+  DATE_REGEX = /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/
+  validates :status, presence: true, inclusion: { in: COHEMP_STATUS, message: "Incorrect status" }
+  validates :date, presence: true, format: { with: DATE_REGEX, message: "please enter keywords in correct format"}
 end
--- app/models/cohemp.rb
+++ app/models/cohemp.rb
@@ -1,3 +1,6 @@
 class Cohemp < ApplicationRecord
   belongs_to :user
+  validates :addess, presence: true
+  validates :description, presence: true
+  validates :availability, presence: true, :inclusion => { :in => [true, false] }
 end
