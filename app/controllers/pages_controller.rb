2267366699ab1b4bae7d6a811fbe49dd89e4d013
Roberto Caffaro <caffaro.filho@gmail.com>
2020-10-31 15:21:16 -0300

    Agora é possível aprovar e cancelar bookings de acordo com os usuarios

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,16 @@ class PagesController < ApplicationController
   def profile
     @cohemps = Cohemp.where(user: current_user)
     @bookings = Booking.where(user: current_user)
-    @reservations = Booking.joins(:cohemp).where('cohemps.user_id' == current_user.id)
+    @reservations = Booking.joins(:cohemp).where('cohemps.user_id = ?', current_user.id)
     @user = current_user
   end
 end
+
+ # def profile
+  #  @cohemps = Cohemp.where(user: current_user)
+   # @bookings = Booking.where(user: current_user)
+ #   query = 'INNER JOIN "cohemps" ON "cohemps"."id" = "bookings"."cohemp_id" WHERE "cohemps"."user_id" = ' + current_user.id.to_s
+  #  @reservations = Booking.joins(query)
+  #  @user = current_user
+ #   binding.pry
+ # end
