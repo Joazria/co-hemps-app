f585a09bf9375c15bcf7fb9e0c363b882213fbe3
Wallace Rezende <wallacerezende12@gmail.com>
2020-11-05 20:01:35 -0300

    added the cloudinary configuration

--- app/models/cohemp.rb
+++ app/models/cohemp.rb
@@ -6,4 +6,5 @@ class Cohemp < ApplicationRecord
   validates :availability, presence: true, :inclusion => { :in => [true, false] }
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_many_attached :photos
 end
