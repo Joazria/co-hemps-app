d0d23b2f59efdcf7a048a34ce1d8fbb2d00d74a0
Roberto Caffaro <caffaro.filho@gmail.com>
2020-11-07 13:57:06 -0300

    Fixed availability

--- app/models/cohemp.rb
+++ app/models/cohemp.rb
@@ -3,7 +3,7 @@ class Cohemp < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :description, presence: true
-  validates :availability, presence: true, :inclusion => { :in => [true, false] }
+  # validates :availability, presence: true, :inclusion => { :in => [true, false] }
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   has_many_attached :photos
